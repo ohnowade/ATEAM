a4b4e73c5cd6b478ef98819679eda4ce54cc6ebe
jiayu <jiayu@DESKTOP-25NRRE9>
2019-04-23 22:31:42 -0500

    start gui

--- Final Project/src/application/Start.java
+++ Final Project/src/application/Start.java
@@ -1,11 +1,14 @@
 package application;
 
 
+
 import java.util.Collection;
+import javax.swing.border.TitledBorder;
 // import java.awt.TextField;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -16,6 +19,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 
 public class Start extends Application {
@@ -23,10 +28,14 @@ public class Start extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Label title = new Label("Start");
+        
+         setTitle(title);
         Label topicLabel = new Label("Topic: ");
         Label questionLabel = new Label("Number of questions: ");
+       
         Button starButton = new Button("Start Quiz");
         Button backButton = new Button("Back to Menu");
+        SetButton(starButton, backButton);
 
         ObservableList<String> options = FXCollections.observableArrayList("Option 1", "Option 2",
                         "Option 3", "Option 4", "Option 5");
@@ -50,17 +59,13 @@ public class Start extends Application {
         rightBox.getChildren().addAll(questionLabel, numberQ);
         centerVBox.getChildren().addAll(centerBox, rightBox);
 
-        VBox bottomVbox = new VBox();
-        bottomVbox.setAlignment(Pos.CENTER_LEFT);
-        bottomVbox.setSpacing(150);
-        
-        bottomVbox.getChildren().addAll(starButton, backButton);
-
-
-
+        VBox bottomV = new VBox();
+        bottomV.getChildren().addAll(starButton, backButton);
+        setBottomVbox(bottomV);
         root.setTop(title);
         root.setCenter(centerVBox);
-        root.setRight(bottomVbox);
+        root.setRight(bottomV);
+        root.setStyle("-fx-background-color: #000000;");
         
 
         Scene scene = new Scene(root, 1200, 800); // set the scene
@@ -72,7 +77,25 @@ public class Start extends Application {
         primaryStage.show();
 
     }
-
+ public void SetButton(Button starButton, Button backButton) {
+     starButton.setMinSize(200, 50);
+     backButton.setMinSize(200, 50);
+     starButton.setFont(new Font(26));
+     backButton.setFont(new Font(26));
+     starButton.setStyle("-fx-background-color: #FFA31A;");
+     backButton.setStyle("-fx-background-color: #FFA31A;");
+ }
+ public void setBottomVbox(VBox bottomVBox) {
+     bottomVBox.setAlignment(Pos.CENTER_LEFT);
+     bottomVBox.setSpacing(150);
+//    bottomVBox.setMargin
+     
+ }
+ 
+ public void setTitle(Label title) {
+     title.setFont(new Font(100));
+     title.setTextFill(Color.WHITE);
+ }
     public static void main(String[] args) {
         launch(args);
     }
