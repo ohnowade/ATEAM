ef012b711ceae462905be4d00f517b5716acb59f
ohnowade <ctan46@wisc.edu>
2019-04-24 01:20:19 -0500

    quiz

--- Final Project/src/application/Quiz.java
+++ Final Project/src/application/Quiz.java
@@ -11,8 +11,10 @@ import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import java.util.*;
 
 public class Quiz extends Application{
 
@@ -20,44 +22,83 @@ public class Quiz extends Application{
 	public void start(Stage primaryStage) {
 		// the label indicating the question
 		Label question = new Label("question");
-		
+
 		// The image of current question
 		Image image = new Image("noImage.jpg");
 		ImageView view = new ImageView(image);
-		view.setFitWidth(500);
+		view.setFitWidth(700);
 		view.setPreserveRatio(true);
-		
-		// the button indicating the completion of this question
-		Button confirm = new Button("Confirm");
-		
+
+		// The label indicating correct answers
+		Label answer = new Label("The correct answer is A");
+		answer.setVisible(false);
+
 		// the choices
+		ArrayList<RadioButton> choices = new ArrayList<RadioButton>();
 		RadioButton first = new RadioButton("first choice");
 		RadioButton second = new RadioButton("second choice");
 		RadioButton third = new RadioButton("third choice");
+		// put all choices in a toggle group so that the user could only select one choice
 		ToggleGroup group = new ToggleGroup();
 		first.setToggleGroup(group);
 		second.setToggleGroup(group);
 		third.setToggleGroup(group);
+		choices.add(first);
+		choices.add(second);
+		choices.add(third);
+
+		// the button indicating the completion of this question
+		Button confirm = new Button("Confirm");
+		// Before the user makes selection, it would be disabled
+		confirm.setDisable(true);
+		// if the user makes any selection, it would be able to be clicked
+		for (RadioButton temp : choices) {
+			temp.setOnAction(e -> {
+				confirm.setDisable(false);
+			}); 
+		}
+		// After the user clicks "confirm", the answer would be checked and shown, the button would lead the
+		// user to the next question
+		if (confirm.getText().equals("Confirm")) {
+			confirm.setOnMouseClicked( e -> {
+				confirm.setText("Next");
+				answer.setVisible(true);
+			});
+		} else {
+			confirm.setOnMouseClicked( e -> {
+				Stage mainStage = new Stage();
+				Main main = new Main();
+				main.start(mainStage);
+			});
+		}
+
+		// set the layout
+		HBox hbox = new HBox(answer, confirm);
+		HBox.setMargin(confirm, new Insets(0, 0, 0, 700));
+
+		VBox vbox = new VBox(question, view);
+		for (int i = 0; i < choices.size(); i++) {
+			vbox.getChildren().addAll(choices.get(i));
+		}
 		
-		VBox vbox = new VBox(question, view, first, second, third, confirm);
+		vbox.getChildren().add(hbox);
 		VBox.setMargin(question, new Insets(20, 0, 0, 10));
 		VBox.setMargin(view, new Insets(0, 0, 0, 10));
-		VBox.setMargin(confirm, new Insets(100, 0, 0, 900));
+		VBox.setMargin(hbox, new Insets(50, 0, 0, 0));
 
 		vbox.setSpacing(30);
 		vbox.setAlignment(Pos.BASELINE_LEFT);
 
 		BorderPane root = new BorderPane();
 		root.setCenter(vbox);
-		
+
 		Scene scene = new Scene(root,1200,800);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Quiz");
 		primaryStage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
