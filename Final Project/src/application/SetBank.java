da18d50f45b1552d99f9d59ae405aea0c21c7c88
jiayu <jiayu@DESKTOP-25NRRE9>
2019-04-24 01:34:46 -0500

    start and setBank

--- Final Project/src/application/SetBank.java
+++ Final Project/src/application/SetBank.java
@@ -1,15 +1,78 @@
 package application;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.*;
 
-public class SetBank extends Application{
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
+public class SetBank extends Application {
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            Image image = new Image("setQuestion.jpg");// create image node with my photo
+            ImageView title = new ImageView(image);
+            title.setFitWidth(500); // set the size of the image title
+            title.setPreserveRatio(true);
+
+            Label warning = new Label("Load From File :");  // create required button
+
+            Button confirmB = new Button("Confirm");
+            Button addQ = new Button("Add new Question");
+            Button back = new Button("Back to Menu");
+
+            TextField pathField = new TextField(); // create textfield for file path entering
+
+            HBox CBox = new HBox();   // hbox for file path entering
+            CBox.getChildren().addAll(warning, pathField, confirmB);
+            CBox.setAlignment(Pos.CENTER);
+            CBox.setSpacing(40);
+
+
+             // Vbox for the two button and the HBox
+            VBox centerVBox = new VBox();
+            centerVBox.getChildren().addAll(CBox, addQ, back);
+            centerVBox.setAlignment(Pos.CENTER);
+            VBox.setMargin(CBox, new Insets(20, 0, 70, 0));
+            centerVBox.setSpacing(30);
+
+
+
+            BorderPane root = new BorderPane();
+            root.setTop(title); // set borderPane
+            BorderPane.setAlignment(title, Pos.CENTER);
+            BorderPane.setMargin(title, new Insets(110, 0, 0, 0));
+            BorderPane.setMargin(centerVBox, new Insets(20, 0, 0, 0));
+
+            root.setCenter(centerVBox);
+
+
+            Scene scene = new Scene(root, 800, 600); // set the scene
+
+
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
--- Final Project/src/application/Start.java
+++ Final Project/src/application/Start.java
@@ -24,70 +24,71 @@ public class Start extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-try {
-        Image image = new Image("start.png");// create image node with my photo
-        ImageView titleView = new ImageView(image);
-        titleView.setFitWidth(250); // adjust the size of the photo
-        titleView.setPreserveRatio(true);
+        try {
+            Image image = new Image("start.png");// create image node with my photo
+            ImageView titleView = new ImageView(image);
+            titleView.setFitWidth(250); // adjust the size of the photo
+            titleView.setPreserveRatio(true);
 
-        Label topicLabel = new Label("Topic: ");
-        Label questionLabel = new Label("Number of questions: ");
-
-
-        Button starButton = new Button("Start Quiz");
-        Button backButton = new Button("Back to Menu");
-
-        ObservableList<String> options = FXCollections.observableArrayList("Option 1", "Option 2",
-                        "Option 3", "Option 4", "Option 5");
-        @SuppressWarnings({"rawtypes", "unchecked"})
-        ComboBox comboBox = new ComboBox(options);
-        setComBox(comboBox);
-
-        TextField numberQ = new TextField();
-
-        HBox centerBox = new HBox();
-        centerBox.setAlignment(Pos.CENTER);
-        centerBox.getChildren().addAll(topicLabel, comboBox);
-
-        HBox rightBox = new HBox();
-        rightBox.setAlignment(Pos.CENTER);
-        rightBox.getChildren().addAll(questionLabel, numberQ);
-
-        VBox centerV = new VBox();
-        centerV.getChildren().addAll(centerBox, rightBox);
-        setCenterVbox(centerV);
+            Label topicLabel = new Label("Topic: ");
+            Label questionLabel = new Label("Number of questions: ");
 
 
-        VBox bottomV = new VBox();
-        bottomV.getChildren().addAll(starButton, backButton);
-        setBottomVbox(bottomV);
+            Button starButton = new Button("Start Quiz");
+            Button backButton = new Button("Back to Menu");
 
-        BorderPane root = new BorderPane();
-        root.setTop(titleView);
-        root.setCenter(centerV);
-        root.setBottom(bottomV);
+            ObservableList<String> options = FXCollections.observableArrayList("Option 1",
+                            "Option 2", "Option 3", "Option 4", "Option 5");
+            @SuppressWarnings({"rawtypes", "unchecked"})
+            ComboBox comboBox = new ComboBox(options);
+            setComBox(comboBox);
 
-        root.setStyle("-fx-background-color: #000000;");
-        BorderPane.setAlignment(centerBox, Pos.TOP_CENTER);
-        BorderPane.setAlignment(titleView, Pos.BOTTOM_CENTER);
+            TextField numberQ = new TextField();
 
-        Scene scene = new Scene(root, 800, 600); // set the scene
+            HBox centerBox = new HBox();
+            centerBox.setAlignment(Pos.CENTER);
+            centerBox.getChildren().addAll(topicLabel, comboBox);
+
+            HBox rightBox = new HBox();
+            rightBox.setAlignment(Pos.CENTER);
+            rightBox.getChildren().addAll(questionLabel, numberQ);
+
+            VBox centerV = new VBox();
+            centerV.getChildren().addAll(centerBox, rightBox);
+            setCenterVbox(centerV);
 
 
-        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-        primaryStage.setScene(scene);
-        primaryStage.show();
+            VBox bottomV = new VBox();
+            bottomV.getChildren().addAll(starButton, backButton);
+            setBottomVbox(bottomV);
 
-    }catch(Exception e) {
-        
+            BorderPane root = new BorderPane();
+            root.setTop(titleView);
+
+            root.setCenter(centerV);
+            root.setBottom(bottomV);
+
+            root.setStyle("-fx-background-color: #000000;");
+            BorderPane.setMargin(titleView, new Insets(50, 0, 0, 0));
+            BorderPane.setAlignment(centerBox, Pos.TOP_CENTER);
+            BorderPane.setAlignment(titleView, Pos.BOTTOM_CENTER);
+
+            Scene scene = new Scene(root, 800, 600); // set the scene
+
+
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    }
-
 
     public void setBottomVbox(VBox bottomVBox) {
         bottomVBox.setAlignment(Pos.BOTTOM_CENTER);
         bottomVBox.setSpacing(80);
-        bottomVBox.setPadding(new Insets(20, 70, 70, 70));
+        bottomVBox.setPadding(new Insets(0, 70, 70, 70));
 
 
     }
@@ -95,7 +96,7 @@ try {
     public void setCenterVbox(VBox centerVBox) {
         centerVBox.setAlignment(Pos.CENTER);
         centerVBox.setSpacing(60);
-        centerVBox.setPadding(new Insets(10, 40, 40, 40));
+        centerVBox.setPadding(new Insets(10, 0, 40, 40));
     }
 
 
