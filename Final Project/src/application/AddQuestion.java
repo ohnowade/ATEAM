0e00b16fe1f4c952a3d9522a9e33468b03fa0b87
ohnowade <ctan46@wisc.edu>
2019-04-24 13:11:29 -0500

    add

--- Final Project/src/application/AddQuestion.java
+++ Final Project/src/application/AddQuestion.java
@@ -2,23 +2,29 @@ package application;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollBar;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.VBox; 
 
 public class AddQuestion extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		try {
+			// The scroll bar to read the complete scene
+			ScrollBar bar = new ScrollBar();
+			bar.setOrientation(Orientation.VERTICAL);
+			
 			// The image for the head of this scene
 			Image image = new Image("addQuestion.png");
 			ImageView head = new ImageView(image);
@@ -28,32 +34,34 @@ public class AddQuestion extends Application{
 			// prompt for topic
 			Label topic = new Label("Topic: ");
 			TextField topicField = new TextField();
-			topicField.minWidth(200);
+			topicField.setPrefWidth(200);
 			HBox topicBox = new HBox(topic, topicField);
 			
-			// prompt for topic
+			// prompt for question
 			Label question = new Label("Question: ");
 			TextField questionField = new TextField();
-			questionField.minWidth(200);
+			questionField.setPrefWidth(200);
 			HBox questionBox = new HBox(question, questionField);
 			
-			// prompt for topic
+			// prompt for the address of image
 			Label imageLabel = new Label("Image Address: ");
 			TextField imageField = new TextField();
-			imageField.minWidth(200);
+			imageField.setPrefWidth(200);
 			HBox imageBox = new HBox(imageLabel, imageField);
 			
-			// prompt for topic
+			// prompt for correct answer of the question to be added
 			Label correctAnswer = new Label("Correct Answer: ");
 			TextField correctField = new TextField();
-			correctField.minWidth(200);
+			correctField.setPrefWidth(200);
 			HBox correctBox = new HBox(correctAnswer, correctField);
 			
-			// prompt for topic
-			Label otherAnswers = new Label("Other Answers: ");
+			// prompt for other choices
+			Label otherChoices = new Label("Other Choices: ");
 			TextField otherField = new TextField();
-			otherField.minWidth(200);
-			HBox otherBox = new HBox(otherAnswers, otherField);
+			Button addChoices = new Button("Add Choices");
+			otherField.setPrefWidth(200);
+			HBox otherBox = new HBox(otherChoices, otherField, addChoices);
+			HBox.setMargin(addChoices, new Insets(0, 0, 0, 235));
 			
 			// two buttons offering choices for the user to go back or to confirm to add
 			Button back = new Button("Back");
@@ -69,8 +77,21 @@ public class AddQuestion extends Application{
 			vbox.getChildren().addAll(head, topicBox, questionBox, imageBox, correctBox, otherBox, buttons);
 			VBox.setMargin(head, new Insets(20, 0, 0, 0));
 			VBox.setMargin(buttons, new Insets(40, 0, 0, 0));
+			
+			// If user chooses to add more choices
+			addChoices.setOnMouseClicked(
+					e -> {
+						TextField extraChoice = new TextField();
+						extraChoice.setPrefWidth(200);
+						HBox newChoice = new HBox(extraChoice);
+						HBox.setMargin(extraChoice, new Insets(0, 0, 0, otherChoices.getWidth()));
+						vbox.getChildren().add(vbox.getChildren().size() - 1, newChoice);
+					});
+			
+			
 			BorderPane root = new BorderPane();
 			root.setCenter(vbox);
+			root.setRight(bar);
 			
 			Scene scene = new Scene(root,800,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
