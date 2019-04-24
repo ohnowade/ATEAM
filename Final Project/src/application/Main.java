9ecb7fe30f60b5883cb4af9f4392261cdfa69d9c
ohnowade <ctan46@wisc.edu>
2019-04-23 23:15:08 -0500

    main

--- Final Project/src/application/Main.java
+++ Final Project/src/application/Main.java
@@ -1,13 +1,17 @@
 package application;
 	
+import java.awt.Paint;
+
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -22,9 +26,6 @@ public class Main extends Application {
 			Button start = new Button("Start");
 			Button setUp = new Button("Set Up");
 			Button exit = new Button("Exit");
-			
-			// set up all the buttons
-			setButtons(start, setUp, exit);
 
 			// The image for the head of this scene
 			Image image = new Image("headMain.png");
@@ -36,12 +37,13 @@ public class Main extends Application {
 			vbox.setAlignment(Pos.BASELINE_CENTER);
 			vbox.getChildren().addAll(head, start, setUp, exit);
 			VBox.setMargin(head, new Insets(20, 0, 0, 0));
-			vbox.setStyle("-fx-background-color: #000000;");
+			//vbox.setStyle("-fx-background-color: #000000;");
 			BorderPane root = new BorderPane();
 			root.setCenter(vbox);
 			
 			Scene scene = new Scene(root,800,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -49,26 +51,6 @@ public class Main extends Application {
 		}
 	}
 	
-	/**
-	 * This helps set up the size, font, and background color of all buttons
-	 * @param start - the start button
-	 * @param setUp - the set up button
-	 * @param exit - the exit button
-	 */
-	private void setButtons(Button start, Button setUp, Button exit) {
-		// set the size of all buttons to 200*50
-		start.setMinSize(200, 50);
-		setUp.setMinSize(200, 50);
-		exit.setMinSize(200, 50);
-		// set the font of all buttons
-		start.setFont(new Font(26));
-		setUp.setFont(new Font(26));
-		exit.setFont(new Font(26));
-		// set background color of all buttons
-		start.setStyle("-fx-background-color: #FFA31A;");
-		setUp.setStyle("-fx-background-color: #FFA31A;");
-		exit.setStyle("-fx-background-color: #FFA31A;");
-	}
 	
 	public static void main(String[] args) {
 		launch(args);
