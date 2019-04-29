cb20a56862d65576742b7c0c941f71eff25c7c98
oudon <oudon@DESKTOP-GQU8ODH>
2019-04-28 22:43:27 -0500

    quiz

--- Final Project/src/data/Quiz.java
+++ Final Project/src/data/Quiz.java
@@ -1,5 +1,36 @@
 package data;
 
-public class Quiz {
+import java.util.Stack;
 
+/**
+ * This class serve to support the program function during quiz period. Function
+ * includes:
+	1. Show question for the quiz 
+	2. Show answer for the quiz 
+	3. Compare answer show correctness 
+	4. Keep counts of correctness
+ * 
+ * @author oudon
+ *
+ */
+public class Quiz {
+	int numCorrect;
+	int numAnswered;
+	Stack<Question> question;
+
+	/**
+	 * default constructor
+	 */
+	public Quiz() {
+
+	}
+
+	/**
+	 * Check if the question is correct;
+	 * 
+	 * @return
+	 */
+	boolean checkCorrect() {
+		return true;
+	}
 }
